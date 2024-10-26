bac76bb07fd2dae09279c03f1f8fdf213e47adf2
floracmag <flora.carneiro@mail.polimi.it>
2024-10-26 12:20:54 -0300

    first 26/10

--- sketch.js
+++ sketch.js
@@ -1,17 +1,41 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  noLoop();
 }
 
 function draw() {
-  // put drawing code here
+  background(220);
+
+  noFill();
+  blendMode(BURN);
+
+  stroke("tomato");
+
+  for (let i = 0; i < 5; i++) {
+    push();
+    translate(100 * i, 100);
+    drawLines(random(1, 2), 50, 3); // Define o número de pontos como 3
+    pop();
+  }
 }
+
+function drawLines(numLines = 3, side = 50, points = 3) { // Altera o valor padrão de points para 3
+  for (let n = 0; n < numLines; n++) {  
+    drawLine(side, points);
+  }
+}
+
+function drawLine(side = 100, points = 3) { // Aqui, define points como 3
+  beginShape();
+
+  for (let p = 0; p < points; p++) {
+    let x = map(p, 0, points - 1, -side / 2, side / 2); // Gera x de -side/2 a side/2
+    let y = random(-side / 2, side / 2); // Gera y aleatório entre -side/2 e side/2
+
+    vertex(x, y);
+  }
+
+  endShape();
+}
+
+
